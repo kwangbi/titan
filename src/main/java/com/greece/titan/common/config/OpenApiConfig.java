e636a5ed33b7ed364874809631d4b039815d9aac
kwangbi <kwangbi@naver.com>
2021-04-23 14:35:47 +0900

    sts로 변경

---
+++ src/main/java/com/greece/titan/common/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.greece.titan.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Component
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+    Info info = new Info().title("SCM Order API").version(appVersion)
+            .description("Titan Project API")
+            .termsOfService("http://swagger.io/terms/")
+            .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
+
+    return new OpenAPI()
+            .components(new Components())
+            .info(info);
+  }
+
+}
